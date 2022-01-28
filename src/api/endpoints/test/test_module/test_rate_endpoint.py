219597a10eb5637f5717fb6fd36bdfa0e5ffdcd8
Sorkhemiri <sorkhemiri@gmail.com>
2022-01-28 14:39:41 +0330

    feat: test zero state added

--- src/api/endpoints/test/test_module/test_rate_endpoint.py
+++ src/api/endpoints/test/test_module/test_rate_endpoint.py
@@ -29,3 +29,27 @@ class RateEndPointTestCase:
                 "overall": 7.04,
                 "components": {"energy": 3.277, "time": 2.767, "transaction": 1},
             }
+
+    @staticmethod
+    def test_zero_state():
+        with TestClient(app) as client:
+            data = {
+                "rate": {"energy": 0, "time": 0, "transaction": 0},
+                "cdr": {
+                    "meterStart": 0,
+                    "timestampStart": "2021-04-05T10:04:00Z",
+                    "meterStop": 0,
+                    "timestampStop": "2021-04-05T10:04:00Z",
+                },
+            }
+
+            response = client.post(
+                "/rate",
+                data=json.dumps(data),
+                headers={"Content-Type": "application/json"},
+            )
+            assert 200 == response.status_code
+            assert response.json() == {
+                "overall": 0,
+                "components": {"energy": 0, "time": 0, "transaction": 0},
+            }
