4cf885b4a33559f6965d5e5128aef6c087bb682d
Sorkhemiri <sorkhemiri@gmail.com>
2022-01-28 17:43:17 +0330

    feat: additional validations added to project

--- src/validators/cdr_validator.py
+++ src/validators/cdr_validator.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from fastapi import HTTPException
+from pydantic import BaseModel, validator
+from starlette.status import HTTP_400_BAD_REQUEST
 
 
 class CDRValidator(BaseModel):
@@ -8,3 +10,25 @@ class CDRValidator(BaseModel):
     timestampStart: datetime
     meterStop: int
     timestampStop: datetime
+
+    @validator('meterStart', 'meterStop')
+    def must_be_non_negative(cls, v):
+        if not v >= 0:
+            raise HTTPException(
+                status_code=HTTP_400_BAD_REQUEST,
+                detail="meterStart and meterStop must be non-negative"
+                )
+        return v
+
+    def __init__(self, *args, **kwargs):
+        super(CDRValidator, self).__init__(*args, **kwargs)
+        if self.timestampStart > self.timestampStop:
+            raise HTTPException(
+                status_code=HTTP_400_BAD_REQUEST,
+                detail="timestampStart must be less or equal to timestampStop"
+            )
+        if self.meterStart > self.meterStop:
+            raise HTTPException(
+                status_code=HTTP_400_BAD_REQUEST,
+                detail="meterStart must be less or equal to meterStop"
+            )
--- src/validators/rate_validator.py
+++ src/validators/rate_validator.py
@@ -1,9 +1,20 @@
 from decimal import Decimal
 
-from pydantic import BaseModel
+from fastapi import HTTPException
+from pydantic import BaseModel, validator
+from starlette.status import HTTP_400_BAD_REQUEST
 
 
 class RateValidator(BaseModel):
     energy: Decimal
     time: Decimal
     transaction: Decimal
+
+    @validator('energy', 'time', 'transaction')
+    def must_be_non_negative(cls, v):
+        if not v >= 0:
+            raise HTTPException(
+                status_code=HTTP_400_BAD_REQUEST,
+                detail="meterStart and meterStop must be non-negative"
+            )
+        return v
